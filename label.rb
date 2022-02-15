96b7f0bc1f5ebfb38a2f6a7235f34d438ea765a2
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-15 18:03:31 +0200

    Finish ititialize for method

--- label.rb
+++ label.rb
@@ -1,4 +1,9 @@
 class Label
   attr_reader :id, :title, :color, :items
 
-  def initialize()
+  def initialize(title:, color:, id: 0)
+    @id = !id || id.zero? ? Random.rand(1..1_000_000) : id
+    @title = title
+    @color = color
+    @items = []
+  end
