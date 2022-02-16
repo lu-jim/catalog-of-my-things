0b2a811581430f13da015e363f5d16f6943e9b15
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-16 12:41:29 -0500

    Fix linter issues

--- book.rb
+++ book.rb
@@ -1,10 +1,10 @@
 require './item'
 
-class Book < Item 
-  attr_reader :publisher, :cover_state, :published_date :archived
+class Book < Item
+  attr_reader :publisher, :cover_state, :published_date, :archived
 
-  def initialize(id:, publisher: ,cover_state:, published_date:, archived: false)
-    super(id: id, publisher:, cover_state:, archived: archived)
+  def initialize(publisher, cover_state, published_date, archived: false)
+    super(published_date, publisher:, cover_state:, archived:)
     @publisher = publisher
     @cover_state = cover_state
   end
