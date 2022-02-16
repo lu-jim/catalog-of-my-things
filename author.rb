66162efbe81bd98bd790d3ad5977dc5498c6a621
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-16 13:40:29 -0500

    Create Author classs

---
+++ author.rb
@@ -0,0 +1,11 @@
+class Author
+  attr_accessor :first_name, :last_name
+  attr_reader :id
+
+  def initialize(first_name, last_name, id: Random.rand(1..1_000_000))
+    @id = id
+    @first_name = first_name
+    @last_name = last_name
+    @items = []
+  end
+end
