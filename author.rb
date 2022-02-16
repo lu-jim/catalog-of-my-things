0c564a651c560aa6a1b5332b2b7010e179119284
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-16 16:32:39 -0500

    Use SecureRandom for author id

--- author.rb
+++ author.rb
@@ -1,9 +1,11 @@
+require 'securerandom'
+
 class Author
   attr_accessor :first_name, :last_name
   attr_reader :id
 
-  def initialize(first_name, last_name, id: Random.rand(1..1_000_000))
-    @id = id
+  def initialize(first_name, last_name)
+    @id = SecureRandom.hex(4)
     @first_name = first_name
     @last_name = last_name
     @items = []
