ed4d36837de79a342fead91eca3a1dcf29bcba88
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-17 09:30:03 +0200

    Add changes to book handler

--- book_handler.rb
+++ book_handler.rb
@@ -5,20 +5,19 @@ require './label'
 class BookHandler
   attr_reader :books
 
-
-  def initialize 
+  def initialize
     books = File.read('./json/books.json')
-    @books = books == '' [] : change_hash_to_books(JSON.parse(books))
+    @books = books == '' ? [] : convert_hashs_to_books(JSON.parse(books))
   end
 
   def add_book
-    puts 'Enter pubisher'
+    puts 'Enter publisher'
     publisher = gets.chomp
     puts 'Enter cover state'
     cover_state = gets.chomp
     puts 'Enter publish date'
     publish_date = gets.chomp
-    puts 'Is book archived [y/n]'
+    puts 'Enter is book archived [Y/N] ?'
     archived = gets.chomp
     is_archived = archived.downcase == 'y'
     book = Book.new(id: nil, publisher: publisher, cover_state: cover_state, publish_date: publish_date)
@@ -29,58 +28,69 @@ class BookHandler
   end
 
   def add_label(book)
-    puts 'Enter if you are adding label to this book [y/n]'
-    response = gets.chomp
-    book.add_label if response.downcase == 'y'
+    puts 'Enter if you want to add a label to this book [Y/N] ?'
+    reponse = gets.chomp
+    book.add_label if reponse.downcase == 'y'
   end
 
   def list_all_books
     @books.each_with_index do |b, i|
-      puts "#{i}) Publisher: #{b.publisher},
-      Cover_State: #{b.cover_state},
-      Publish_Date: #{b.publish_date},
-      Archived: #{b.archived}"
-
+      puts "
+       #{i})
+            Publisher: #{b.publisher},
+            Cover State: #{b.cover_state},
+            Publish Date: #{b.publish_date},
+            Archived : #{b.archived}
+        "
       show_label b.label
     end
   end
 
   def save_books
-    hash_arr =change_books_to_hashes
+    hash_arr = convert_books_to_hashes
     json = JSON.generate(hash_arr)
     File.write('./json/books.json', json)
   end
 
   private
 
-  def change_hash_to_books(hashes)
+  def convert_hashs_to_books(hashes)
     books = []
     hashes.each do |h|
-      b = Book.new(id: nil, pubisher: h['publisher'], cover_state: h['cover_state'], publish_date: h['publish_date'], archived: h['archived'])
-      l = change_hashes_to_labels h['label']
+      b = Book.new(id: nil, publisher: h['publisher'], cover_state: h['cover_state'], publish_date: h['publish_date'],
+                   archived: h['archived'])
+      l = convert_hash_to_label h['label']
       b.add_label(l)
       books << b
     end
     books
   end
 
-  def change_hashes_to_labels(hash)
+  def convert_hash_to_label(hash)
     Label.new(id: hash['id'], title: hash['title'], color: hash['color'])
   end
 
-  def change_books_to_hashes
+  def convert_books_to_hashes
     hash_arr = []
     @books.each do |b|
-      hash = { publisher: b.pubisher, cover_state: b.cover_state, publish_date: b.publish_date, archived: b.archived,
-      label: if b.label {id: b.label.id, title: b.title, color: b.label.color} end }
+      hash = { publisher: b.publisher, cover_state: b.cover_state, publish_date: b.publish_date,
+               archived: b.archived, label: if b.label
+                                              { id: b.label.id, title: b.label.title, color: b.label.color }
+                                            end }
       hash_arr.push(hash)
     end
-      hash_arr
+    hash_arr
   end
 
   def show_label(label)
     return unless label.title
-    print "Lable: ID: #{label.id}, Title: #{label.title}, Color: #{label.color} "
+
+    print "
+            Lable:
+              ID: #{label.id},
+              Title: #{label.title},
+              Color: #{label.color}
+      "
   end
 end
 
