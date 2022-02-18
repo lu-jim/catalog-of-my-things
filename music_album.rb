6481744d36f1b0b956825d37c548e7ffd7dc3ba7
mutinhiri <barnabmutinhiri@gmail.com>
2022-02-18 18:14:57 +0200

    Finish Initialize method

--- music_album.rb
+++ music_album.rb
@@ -3,4 +3,7 @@ require_relative 'item'
 class MusicAlbum < Item
   attr_accessor: published_date, :on_spotify, :archived
 
-  def initialize(id:, :published_date)
+  def initialize(id:, :published_date, archived: false, on_spotify: false)
+    super(id: id, published: published_date: published_date, archived: archived)
+    @on_spotify = on_spotify
+  end
