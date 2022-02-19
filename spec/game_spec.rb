69ffa5a6b2069a46e538e3a1bee4396d5ae4f5b5
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-19 06:51:26 -0500

    Add test for game multiplayer

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,15 @@ require './game'
 
 describe Game do
   context 'Given the proper inputs' do
-    game = Game.new(id:nil, published_date: '1984-06-06', multiplayer: false, last_played_at: '2020-12-10')
+    game = Game.new(id: nil, published_date: '1984-06-06', multiplayer: false, last_played_at: '2020-12-10')
+
+    it 'should properly render published date' do
+      expect(game.published_date.to_s).to eq '1984-06-06'
+    end
+
+    it 'should properly render multiplayer' do
+      expect(game.multiplayer).to eq false
+    end
 
     it 'should properly render published date' do
       expect(game.published_date.to_s).to eq '1984-06-06'
