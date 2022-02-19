171d4919cdca1f0e816b8512a322eec31f61e865
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-19 06:50:19 -0500

    Add test for game published date

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,10 @@ require './game'
 
 describe Game do
   context 'Given the proper inputs' do
-    game = Game.new(published_date: '1984-06-06', multiplayer: false, last_played_at: '2020-12-10')
+    game = Game.new(id:nil, published_date: '1984-06-06', multiplayer: false, last_played_at: '2020-12-10')
+
+    it 'should properly render published date' do
+      expect(game.published_date.to_s).to eq '1984-06-06'
+    end
   end
 end
