f2fd7bd88d80b02018794df1768b631431ad9c76
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-02-19 07:16:59 -0500

    Create add author method

--- author_manager.rb
+++ author_manager.rb
@@ -3,6 +3,17 @@ require_relative 'author'
 class AuthorManager
   def initialize
     authors = File.read('./json/authors.json')
-    @authors = authors.nil? ? [] : JSON.parse(author)
+    @author_list = authors.nil? ? [] : JSON.parse(authors)
+  end
+
+  def add_author
+    puts 'Enter First Name'
+    first_name = gets.chomp
+    puts 'Enter Last Name'
+    last_name = gets.chomp
+    author = Author.new(first_name, last_name)
+    @author_list.push(author)
+    save_authors
+    label
   end
 end
